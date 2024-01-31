04299ec24d1f3ad26e8b5a90c25e4a9a5e86294a
anitagaga <anita.gagarina@tixr.com>
2024-01-30 19:31:14 -0800

    Initial Commit

---
+++ HW3.2_APIBiblia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3.2_APIBiblia
+//
+//  Created by Anita Gagarina on 1/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
